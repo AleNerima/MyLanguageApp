2846d4e1b90cbc3bd3faed181bf5c02e2c21a43f
AleNerima <ale95polioni@gmail.com>
2025-03-13 09:59:49 +0100

    progetto backend springboot

---
+++ Backend_SPringBoot/LanguageAppBackend/src/main/java/com/LanguageApp/LanguageAppBackend/LanguageAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LanguageApp.LanguageAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageAppBackendApplication.class, args);
+	}
+
+}
---
+++ Backend_SPringBoot/LanguageAppBackend/src/test/java/com/LanguageApp/LanguageAppBackend/LanguageAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LanguageApp.LanguageAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
